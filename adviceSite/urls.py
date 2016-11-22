cd458edc34ad78b8121b34a7156584374260bca9
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-11-22 00:52:19 +0000

    make front page and interview advice page - both very basic

--- adviceSite/urls.py
+++ adviceSite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.flatpages import views
 
+# Your other patterns here
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^interviews/', include('interview.urls')),
+    url(r'^(?P<url>.*/)$', views.flatpage),
+    url(r'^$', views.flatpage, {'url':'/'}, name="home"),
+    # url(r'^$', views.flatpage, name="default"),
 ]
